35a216183434f9366a13c2224309ac0b7ca36ba6
RinKit6024 <butasan6024@gmail.com>
2022-05-27 20:06:46 +0900

    master

---
+++ src/components/Header.js
@@ -0,0 +1,72 @@
+import * as React from 'react'
+import styled from "styled-components"
+import MenuIcon from '@mui/icons-material/Menu'
+
+function Header() {
+  return (
+    <div>
+        <Container>
+          <a>
+            <img src="logo.svg" alt="" />
+          </a>
+        <Menu>
+          <a href="#">Model S</a>
+          <a href="#">Model 3</a>
+          <a href="#">Model X</a>
+          <a href="#">Model Y</a>
+        </Menu>
+        <RightMenu>
+          <a href="#">Shop</a>
+          <a href="#">Tesla Account</a>
+          <CustomMenu />
+        </RightMenu>
+        </Container>
+    </div>
+  )
+}
+
+export default Header
+
+const Container = styled.div`
+  min-height: 60px;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+  top: 0;
+  left: 0; 
+  right: 0;
+`
+
+
+const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  flex: 1;
+  justify-content: center;
+  a{
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0 10px;
+    flex-wrap: nowrap;
+  }
+
+  @media(max-width: 768px) {
+    display: none; //Under 768px, menu will disappear
+  }
+`
+
+const RightMenu = styled.div`
+  display: flex;
+  align-items: center;
+  a{
+    font-weight: 600;
+    text-transform: uppercase;
+    margin-right: 10px;
+    flex-wrap: nowrap;
+  }
+`
+const CustomMenu = styled(MenuIcon)`
+  cursor: pointer;
+
+`
---
+++ src/components/Home2.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from "styled-components"
+import Section from "./Section"
+
+function Home() {
+
+
+  return(
+    <Container2>
+    </Container2>
+  )
+}
+
+export default Home
+
+
+const Container2 = styled.div`
+   width: 100%;
+   height: 100vh;
+   background: orange;
+   `
